807f4fa5a34fcc57bad2addcbc759670fabe3f88
Ekaterina <ekaterina_r21@mail.ru>
2022-05-16 14:59:24 +0300

    Двумерный массив из вещественных чисел

---
+++ Task47/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+int m =3, n =4;
+double[,]mass = new double [m,n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(1, 10) + new Random().NextDouble();
+    }
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(Math.Round(mass[i, j],1) + " ");
+    }
+Console.WriteLine();
+}
