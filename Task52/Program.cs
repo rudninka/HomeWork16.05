b9761701ea91883adc681c799b8ffeeb2eb990d7
Ekaterina <ekaterina_r21@mail.ru>
2022-05-19 22:26:03 +0300

    Среднее арифметическое столбцов

---
+++ Task52/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int m =4, n =4;
+int[,]mass = new int [m,n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+Console.WriteLine();
+}
+
+double sum = 0;
+double A = 0;
+for (int j = 0; j < mass.GetLength(1); j++)
+{
+    sum = 0;
+    A = 0;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        sum += mass[i, j];
+    }
+A = sum/ m;
+Math.Round(A, 1);
+Console.Write(A + " ");
+}
